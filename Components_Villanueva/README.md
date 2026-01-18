ddbba3cf4fe80ffc91b5e5fcabc826dd790ef519
Prince Mattheus Villanueva <villanueva.prince@iisgalvanimi.edu.it>
2026-01-18 12:04:09 +0000

    creazione applicazione angular

--- Components_Villanueva/README.md
+++ Components_Villanueva/README.md
@@ -1,4 +1,4 @@
-# Villanueva
+# ComponentsVillanueva
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.0.5.
 
--- Components_Villanueva/src/app/app.ts
+++ Components_Villanueva/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Villanueva');
+  protected readonly title = signal('Components_Villanueva');
 }
